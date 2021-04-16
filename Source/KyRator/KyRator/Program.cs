34650339dca685e02df9f59c119ed85d476a89ee
Vitali Dzikavitski <vitaldikov@gmail.com>
2021-04-16 20:46:10 +0300

    initial commit

---
+++ Source/KyRator/KyRator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KyRator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
