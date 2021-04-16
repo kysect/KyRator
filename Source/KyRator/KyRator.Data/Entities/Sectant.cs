fa55dc3522c59e6ea391b8b469aefb4f0c79014e
Vitali Dzikavitski <vitaldikov@gmail.com>
2021-04-16 21:54:35 +0300

    add sectant entity

---
+++ Source/KyRator/KyRator.Data/Entities/Sectant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyRator.Data.Entities
+{
+    public class Sectant
+    {
+        public Guid Id { get; set; }
+        public string Nickname { get; set; }
+        public string FullName { get; set; }
+        public string GitHubProfile { get; set; }
+        public string DiscordProfile { get; set; }
+        public int Points { get; set; }
+    }
+}
