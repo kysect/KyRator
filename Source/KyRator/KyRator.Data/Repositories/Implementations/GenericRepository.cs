f4095bd039c1a0b5d812ee83dee861d24f1452c8
Vitali Dzikavitski <vitaldikov@gmail.com>
2021-04-16 22:27:15 +0300

    add generic repository

---
+++ Source/KyRator/KyRator.Data/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KyRator.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace KyRator.Data.Repositories.Implementations
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(DbContext context, DbSet<TEntity> set)
+        {
+            _context = context;
+            _dbSet = set;
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<TEntity> Get(Func<TEntity, bool> predicate)
+        {
+            return _dbSet.Where(predicate).AsQueryable();
+        }
+        public TEntity FindById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public TEntity Create(TEntity item)
+        {
+            _dbSet.Add(item);
+            _context.SaveChanges();
+            return item;
+        }
+        public TEntity Update(TEntity item)
+        {
+            _dbSet.Update(item);
+            _context.SaveChanges();
+            return item;
+        }
+
+        public void Remove(TEntity item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Source/KyRator/KyRator.Data/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyRator.Data.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity>
+        where TEntity : class
+    {
+        TEntity Create(TEntity item);
+        TEntity FindById(int id);
+        IQueryable<TEntity> Get();
+        IQueryable<TEntity> Get(Func<TEntity, bool> predicate);
+        void Remove(TEntity item);
+        TEntity Update(TEntity item);
+    }
+}
