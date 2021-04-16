3e1c32b52208154d861fa46cf92683569c960b6f
Vitali Dzikavitski <vitaldikov@gmail.com>
2021-04-16 22:20:56 +0300

    add context

---
+++ Source/KyRator/KyRator.Data/Repositories/Implementations/KyRatorContext.cs
@@ -0,0 +1,25 @@
+﻿using KyRator.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyRator.Data.Repositories.Implementations
+{
+    public class KyRatorContext : DbContext
+    {
+        public DbSet<Sectant> Sectants { get; set; }
+
+        public KyRatorContext(DbContextOptions<KyRatorContext> options) : base(options)
+        {
+        }
+
+        public KyRatorContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
